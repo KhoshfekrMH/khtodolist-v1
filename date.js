925a01437f1cd245c2623d3da6bd1b68e5a0b349
Khoshfekr MH <khoshfekr2001@protonmail.com>
2022-08-01 22:54:18 +0430

    Full package of project include CSS and two js and some EJS files

---
+++ date.js
@@ -0,0 +1,22 @@
+//jshint esversion: 8
+exports.getDate = function () {
+    const today = new Date();
+
+    const option = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    return today.toLocaleDateString("en-US", option);
+}
+
+exports.getDay = function () {
+    const today = new Date();
+
+    const option = {
+        weekday: "long",
+    };
+
+    return today.toLocaleDateString("en-US", option);
+}
